f951717f3d511b4343f2c78dfa4d84278a50be1e
Bartłomiej Karaczyn <bartlomiej.karaczyn@gmail.com>
2019-03-27 22:20:58 +0100

    Commit message

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,3 @@
-role :app, %w{s34.mydevil.net}
-role :web, %w{s34.mydevil.net}
-role :db,  %w{s34.mydevil.net}
 server 's34.mydevil.net', user: 'rzemyk87', roles: %w{web app db}
 set :rails_env, :production
 set :deploy_to, '/home/rzemyk87/domains/controlcrm.pl'
